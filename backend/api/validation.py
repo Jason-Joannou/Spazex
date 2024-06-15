9c50db3cd9f2c2d67d2bbd64ea8357a188f14d86
Jason Joannou <jjoannou@bscglobal.com>
2024-06-15 20:58:53 +0200

    Introduce load validation

--- backend/api/validation.py
+++ backend/api/validation.py
@@ -2,7 +2,12 @@ from pydantic import BaseModel
 
 class SpazaInfo(BaseModel):
     spaza_name: str
-    spaza_id: int
     spaza_reg_no: int
     spaza_email: str
     spaza_password: str
+    spaza_loyalty_no: str
+
+class LoanApplication(BaseModel):
+    applicant_name: str
+    applicant_id: int
+    application_amount: float
